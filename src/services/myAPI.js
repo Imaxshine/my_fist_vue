62d4a1feaee27c4a0eb784b51cf84a755f55ad15
Imaxshine <imaxshine9@gmail.com>
2026-05-27 14:40:54 +0300

    Live API

--- src/services/myAPI.js
+++ src/services/myAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const sourceDomain = window.location.protocol + "//" + window.location.hostname;
 const my_api = axios.create({
-    baseURL: 'https://representations-calendars-poor-strain.trycloudflare.com/api', //`${sourceDomain}/api`, //import.meta.env.VITE_LOGIN_API,
+    baseURL: 'https://delivery.iceiy.com/api', //`${sourceDomain}/api`, //import.meta.env.VITE_LOGIN_API,
     timeout: 50000,
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`
